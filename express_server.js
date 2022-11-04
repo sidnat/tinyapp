0db8e6a3f63bc8e8bf0648c53e3ecb86b35bd701
Sid Natarajan <sid.nat@gmail.com>
2022-11-04 05:29:51 +0000

    added basic permission features

--- express_server.js
+++ express_server.js
@@ -45,22 +45,19 @@ const getUserByEmail = (users, email) => {
   return false;
 };
 
+const userExists = (userId) => {
+  return users[userId];
+};
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
 app.get("/urls", (req, res) => {
-  const userId = req.cookies["user_id"];
-  if (!userId) {
-    return res.redirect("/register");
+  const userId = req.cookies['user_id'];
+  const loggedIn = userExists(userId);
+  if (!loggedIn) {
+    return res.redirect("/login");
   }
   const user = users[userId];
   const templateVars = {
@@ -71,7 +68,11 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  const userId = req.cookies["user_id"];
+  const userId = req.cookies['user_id'];
+  const loggedIn = userExists(userId);
+  if (!loggedIn) {
+    return res.redirect("/login");
+  }
   const user = users[userId];
   const templateVars = {
     user,
@@ -81,7 +82,14 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const userId = req.cookies["user_id"];
+  const userId = req.cookies['user_id'];
+  const loggedIn = userExists(userId);
+  if (!loggedIn) {
+    return res.redirect("/login");
+  }
+  if (!urlDatabase[req.params.id]) {
+    return res.send("No url exists for this short url id.");
+  }
   const user = users[userId];
   const templateVars = {
     user,
@@ -93,6 +101,10 @@ app.get("/urls/:id", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
+  const loggedIn = userExists(req.cookies['user_id']);
+  if (!loggedIn) {
+    return res.send("Please register an account to shortify urls.");
+  }
   const newId = generateRandomString();
   urlDatabase[newId] = req.body.longURL;
   res.redirect(`/urls/${newId}`);
@@ -101,6 +113,8 @@ app.post("/urls", (req, res) => {
 app.get("/u/:id", (req, res) => {
   if (urlDatabase[req.params.id]) {
     res.redirect(urlDatabase[req.params.id]);
+  } else {
+    res.send('short url id does not exist');
   }
 });
 
@@ -110,18 +124,27 @@ app.post("/urls/:id/delete", (req, res) => {
 });
 
 app.post("/urls/:id", (req, res) => {
+  const userId = req.cookies['user_id'];
+  const loggedIn = userExists(userId);
+  if (!loggedIn) {
+    return res.redirect("/login");
+  }
   urlDatabase[req.params.id] = req.body.urlId;
   res.redirect("/urls");
 });
 
 app.get("/login", (req, res) => {
+  const loggedIn = userExists(req.cookies['user_id']);
+  if (loggedIn) {
+    return res.redirect("/urls");
+  }
   res.render("urls_login");
 });
 
 app.post("/login", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-  const authorizedUser = getUserByEmail(users, email)
+  const authorizedUser = getUserByEmail(users, email);
   if (!authorizedUser) {
     return res.status(403).send('User with that e-mail cannot be found,');
   } else if (authorizedUser.password !== password) {
@@ -137,6 +160,11 @@ app.post("/logout", (req, res) => {
 });
 
 app.get("/register", (req, res) => {
+  const userId = req.cookies['user_id'];
+  const loggedIn = userExists(userId);
+  if (loggedIn) {
+    return res.redirect("/urls");
+  }
   res.render("urls_register");
 });
 
@@ -156,7 +184,6 @@ app.post("/register", (req, res) => {
   };
   res.cookie('user_id', newUserId);
   res.redirect("/urls");
-  console.log(users)
 });
 
 app.listen(PORT, () => {
